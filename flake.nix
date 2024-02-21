09e198b1d3b5d5d4cba8ea5d829c9d69ec33d200
nu11z <nu11z@blackwall.box>
2024-02-21 17:22:50 +0300

    add systemSettings

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   description = "NixOS flake";
 
+  # ---- SYSTEM SETTINGS ---- #
+  systemSettings = {
+    system = "x86_64-linux";
+    profile = "vm";
+    hostname = "hostname";
+    timezone = "Europe/Moscow";
+    locale = "en_US.UTF-8";
+  };
+
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
@@ -9,7 +18,7 @@
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
